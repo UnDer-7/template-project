723940bf336a18c7eb61dcb4c8f002f1c946ae76
UnDer-7 <mateus7532@gmail.com>
2019-02-10 15:50:52 -0200

    routes

--- src/routes.js
+++ src/routes.js
@@ -23,15 +23,6 @@ routes.get(`${rootUrl}/users`, handle(controllers.UserController.getAllUser))
 routes.get(`${rootUrl}/user/:id`, handle(controllers.UserController.getUser))
 routes.delete(`${rootUrl}/user/:id`, handle(controllers.UserController.deleteUser))
 
-/**
- *  TEMPLATE'S ROUTES
- */
-routes.post(`${rootUrl}/template`, validate(validators.TemplateValidator), handle(controllers.TemplateController.createTemplate))
-routes.put(`${rootUrl}/template/:id`, validate(validators.TemplateValidator), handle(controllers.TemplateController.updateTemplate))
-routes.get(`${rootUrl}/template`, handle(controllers.TemplateController.getAllTemplate))
-routes.get(`${rootUrl}/template/:id`, handle(controllers.TemplateController.getTemplate))
-routes.delete(`${rootUrl}/template/:id`, handle(controllers.TemplateController.deleteTemplate))
-
 // Do not remove this cometary
 // ===== yeoman hook =====
 module.exports = routes
